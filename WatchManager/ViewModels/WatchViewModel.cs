c0145469ce15d902b35706e671187c848c1281ec
geras <gera.sukhomlin@gmail.com>
2021-08-12 23:45:35 +0200

    Created "TripleCheckBox"

--- WatchManager/ViewModels/WatchViewModel.cs
+++ WatchManager/ViewModels/WatchViewModel.cs
@@ -5,20 +5,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using WatchManager.Commands;
 using WatchManager.Stores;
 
 namespace WatchManager.ViewModels
 {
     public class WatchViewModel : BaseViewModel
     {
-        public string TestColumn1 { get; } = "Column 1";
-        public string TestColumn2 { get; } = "Column 2";
-        public ObservableCollection<string> TitleCollection { get; set; }
+        private bool _isFilmsSelected;
+        private bool _isSerialsSelected;
+        private bool _isAnimeSelected;
+
+        public bool IsFilmsSelected
+        {
+            get => _isFilmsSelected;
+            set
+            {
+                _isFilmsSelected = value;
+                OnPropertyChanged(nameof(IsFilmsSelected));
+            }
+        }
+        public bool IsSerialsSelected
+        {
+            get => _isSerialsSelected;
+            set
+            {
+                _isSerialsSelected = value;
+                OnPropertyChanged(nameof(IsSerialsSelected));
+            }
+        }
+        public bool IsAnimeSelected
+        {
+            get => _isAnimeSelected;
+            set
+            {
+                _isAnimeSelected = value;
+                OnPropertyChanged(nameof(IsAnimeSelected));
+            }
+        }
+
+        public ObservableCollection<string> RowCollection { get; set; }
+
 
         public WatchViewModel(NavigationStore navigationStore)
         {
-            TitleCollection = new() { "Naruto: Shippuden", "JOJO: Bizzare Adventure", "Hunter x Hunter" };
+            RowCollection = new() { 
+                "Naruto: Shippuden",
+                "JOJO: Bizzare Adventure",
+                "Hunter x Hunter",
+                "Интерстеллар",
+                "Карты, деньги, два ствола",
+                "Остров проклятых",
+                "Гримм",
+                "Доктор Хаус",
+                "Локи"
+            };
         }
+
+
 
     }
 }
