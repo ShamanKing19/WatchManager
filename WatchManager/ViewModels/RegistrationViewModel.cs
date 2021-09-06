18d213ac1955fb4d20b7b95626a7714db5c89fa2
George Sukhomlin <gera.sukhomlin@gmail.com>
2021-09-06 16:40:55 +0200

    Created basic files to make registration

---
+++ WatchManager/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchManager.ViewModels
+{
+    public class RegistrationViewModel : BaseViewModel
+    {
+        private string _login;
+        private string _password;
+        private string _email;
+
+        public string Login
+        {
+            get => _login;
+            set => _login = value;
+        }
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+    }
+}
