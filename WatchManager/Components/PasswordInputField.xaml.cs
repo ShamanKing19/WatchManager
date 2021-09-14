a6160c7d233d6b8621f19f9417a5c91c67584cea
George Sukhomlin <gera.sukhomlin@gmail.com>
2021-09-14 09:55:02 +0200

    Beautified authentication page

---
+++ WatchManager/Components/PasswordInputField.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WatchManager.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordInputField.xaml
+    /// </summary>
+    public partial class PasswordInputField : UserControl
+    {
+        private bool _isPasswordChanging;
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(PasswordInputField),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    PasswordPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+
+        private static void PasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordInputField passwordBox)
+            {
+                passwordBox.UpdatePassword();
+            }
+        }
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public PasswordInputField()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            _isPasswordChanging = true;
+            Password = passwordBox.Password;
+            _isPasswordChanging = false;
+        }
+
+        private void UpdatePassword()
+        {
+            if (!_isPasswordChanging)
+            {
+                passwordBox.Password = Password;
+            }
+        }
+    }
+}
